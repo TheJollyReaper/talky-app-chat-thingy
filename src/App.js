238567fb8aa19a9a9b4a12aeca318d5e0bcdeeea
Edgar Lopez <edgarlopez633@gmail.com>
2021-02-01 16:29:27 -0800

    new messages now get added as new lines. crt background and filter. layout improved misc. colors and schemes. timestamps

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import TextInput from './TextInput';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <div className="logo">
-          Talky app thing
+  const [messages,setMessages] = useState([{text:'Welcome!', date:new Date().toLocaleString()}])
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      APP CHAT THINGY TO CHAT ABOUT STUFF VERSION 72.5 BETA A2 ULTRA
+      <div className="logo" />
+    </header>
+  
+    <main className="messages">
+        <div className="message">
+          {/* {messages[0].text} */}
+          {messages.map((message) => 
+            <div className="message-row">
+              <h2>{message.text}</h2>
+              <p className="date">{message.date}</p>
+            </div>
+            )}
         </div>
-      </header>
-    </div>
-  );
+    </main>
+
+    <TextInput 
+      send={(t)=> setMessages([...messages, {text:t, date:new Date().toLocaleString()}])}
+    />
+  </div>
 }
 
 export default App;
